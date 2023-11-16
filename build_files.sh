f03273e19033fb28201b8f786a73ea0093c54f2f
kartik <Kartikrajput77041@gmail.com>
2023-11-16 21:02:59 +0530

    hsb

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 
 echo "Build start"
-python3.9 -m pip install -r requirement.txt
+python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 echo " Build END" 
--- mac/settings.py
+++ mac/settings.py
@@ -116,11 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Managing media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static'),
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static'),
